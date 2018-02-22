c55f3d24679e96bf0bdb17d08f75115b50857ab8
hal-rock <halsrrockwell@gmail.com>
2018-02-22 17:15:35 -0500

    fixed example to comply with PEP-8
    Made a couple of minor style changes to the example (just
    comments and trailing whitespace) that I missed the first
    time.

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 """
-Example usage of Markov chain (and currently only) composer. 
+Example usage of Markov chain (and currently only) composer.
 """
 
 from markov import Markov
@@ -12,12 +12,12 @@ pieces = ["mond_1.mid", "mond_2.mid", "mond_3.mid"]
 # exactly copying the first-order transitions
 beethoven = Markov()
 beethoven.train(fnames=pieces)
-beethoven.compose("less_random.mid", length=300, delta=400) # 2 minutes long
+beethoven.compose("less_random.mid", length=300, delta=400)  # 2 minutes long
 
 # with a strongish prior bias towards randomness -- sounds worse, I think
 badhoven = Markov(priors=200)
 badhoven.train(fnames=pieces)
-beethoven.compose("more_random.mid", length=200, delta=300) # 1 minute long
+beethoven.compose("more_random.mid", length=200, delta=300)  # 1 minute long
 
 # now you can play the output files with whatever midi player you have
 # though a first-order Markov model really doesn't sound very good
